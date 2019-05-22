d244d63f0bcbb5043eb5ed06a3df1dc84101885a
Ryan Pepper <ryan.pepper@soton.ac.uk>
2019-05-22 14:32:17 +0100

    Alter the expansion order

--- example/fmm-dipole/example.py
+++ example/fmm-dipole/example.py
@@ -1,7 +1,7 @@
 import fmmgen
 
-order = 11
-cse = True
+order = 8
+cse = False
 
 fmmgen.generate_code(order, "operators", CSE=cse, generate_cython_wrapper=False,
                      include_dir="include", src_dir="src")
