39bac24433c222c03bfc24ea595d3865a116d6f5
Ryan Pepper <ryan.pepper@onyxinsight.com>
2022-10-25 21:27:25 +0100

    Run black on the tests

--- tests/test_M_shift.py
+++ tests/test_M_shift.py
@@ -3,41 +3,62 @@ import fmmgen.expansions as exp
 from fmmgen.utils import q, Nterms
 import sympy as sp
 
-x, y, z = sp.symbols('x y z')
+x, y, z = sp.symbols("x y z")
 symbols = (x, y, z)
+
 
 def test_M_shift_monopole():
     order = 2
     source = 0
     array_length = Nterms(order) - Nterms(source - 1)
-    
-    M = sp.MatrixSymbol('M', Nterms(order), 1)
-    M_dict, _ = gen.generate_mappings(order, symbols, key='grevlex', source_order=source)
+
+    M = sp.MatrixSymbol("M", Nterms(order), 1)
+    M_dict, _ = gen.generate_mappings(
+        order, symbols, key="grevlex", source_order=source
+    )
     # Check monopole term
     assert exp.M_shift((0, 0, 0), order, symbols, M_dict, source_order=source) == M[0]
     # Check dipole terms
-    assert exp.M_shift((1, 0, 0), order, symbols, M_dict, source_order=source) == M[1] + M[0]*x
-    assert exp.M_shift((0, 1, 0), order, symbols, M_dict, source_order=source) == M[2] + M[0]*y
-    assert exp.M_shift((0, 0, 1), order, symbols, M_dict, source_order=source) == M[3] + M[0]*z
+    assert (
+        exp.M_shift((1, 0, 0), order, symbols, M_dict, source_order=source)
+        == M[1] + M[0] * x
+    )
+    assert (
+        exp.M_shift((0, 1, 0), order, symbols, M_dict, source_order=source)
+        == M[2] + M[0] * y
+    )
+    assert (
+        exp.M_shift((0, 0, 1), order, symbols, M_dict, source_order=source)
+        == M[3] + M[0] * z
+    )
     # Check quadrupole terms
-    assert exp.M_shift((2, 0, 0), order, symbols, M_dict, source_order=source) ==  \
-        x**2/2 * M[0] + x * M[1] + M[4]
+    assert (
+        exp.M_shift((2, 0, 0), order, symbols, M_dict, source_order=source)
+        == x**2 / 2 * M[0] + x * M[1] + M[4]
+    )
 
-    assert sp.expand(exp.M_shift((1, 1, 0), order, symbols, M_dict, source_order=source)) == x*y*M[0] +  y * M[1] + x * M[2] + M[5]
+    assert (
+        sp.expand(exp.M_shift((1, 1, 0), order, symbols, M_dict, source_order=source))
+        == x * y * M[0] + y * M[1] + x * M[2] + M[5]
+    )
 
-    assert sp.expand(exp.M_shift((1, 0, 1), order, symbols, M_dict, source_order=source)) == x*z*M[0] + z * M[1] + x*M[3] + M[6]
-        
-    
+    assert (
+        sp.expand(exp.M_shift((1, 0, 1), order, symbols, M_dict, source_order=source))
+        == x * z * M[0] + z * M[1] + x * M[3] + M[6]
+    )
+
 
 def test_M_shift_dipole():
     order = 2
     source = 1
-    M = sp.MatrixSymbol('M', Nterms(order), 1)
-    M_dict, _ = gen.generate_mappings(order, symbols, key='grevlex', source_order=source)
+    M = sp.MatrixSymbol("M", Nterms(order), 1)
+    M_dict, _ = gen.generate_mappings(
+        order, symbols, key="grevlex", source_order=source
+    )
 
     # With a source order of 1, the monopole term is no longer produced. We should check
     # therefore, that M_shift returns an error.
-    
+
     print(M_dict[(1, 0, 1)])
 
     try:
@@ -45,70 +66,75 @@ def test_M_shift_dipole():
         raise AssertionError("No exception raised!")
     except ValueError:
         pass
-    
+
     assert exp.M_shift((1, 0, 0), order, symbols, M_dict, source_order=source) == M[0]
     assert exp.M_shift((0, 1, 0), order, symbols, M_dict, source_order=source) == M[1]
     assert exp.M_shift((0, 0, 1), order, symbols, M_dict, source_order=source) == M[2]
 
-    assert sp.expand(exp.M_shift((2, 0, 0), order, symbols, M_dict, source_order=source)) == M[3] + M[0]*x
+    assert (
+        sp.expand(exp.M_shift((2, 0, 0), order, symbols, M_dict, source_order=source))
+        == M[3] + M[0] * x
+    )
 
-    assert sp.expand(exp.M_shift((1, 1, 0), order, symbols, M_dict, source_order=source)) == y * M[0] + x * M[1] + M[4]
+    assert (
+        sp.expand(exp.M_shift((1, 1, 0), order, symbols, M_dict, source_order=source))
+        == y * M[0] + x * M[1] + M[4]
+    )
 
-    assert sp.expand(exp.M_shift((1, 0, 1), order, symbols, M_dict, source_order=source)) == z * M[0] + x*M[2] + M[5]
-
+    assert (
+        sp.expand(exp.M_shift((1, 0, 1), order, symbols, M_dict, source_order=source))
+        == z * M[0] + x * M[2] + M[5]
+    )
 
 
 def test_generate_M_shift_operators_monopole():
     order = 1
     source = 0
 
-    M_dict = {(0, 0, 0): 0,
-              (1, 0, 0): 1,
-              (0, 1, 0): 2,
-              (0, 0, 1): 3}
-    
-    M = sp.MatrixSymbol('M', 4, 1)
-    x, y, z = sp.symbols('x y z')
+    M_dict = {(0, 0, 0): 0, (1, 0, 0): 1, (0, 1, 0): 2, (0, 0, 1): 3}
 
-    answer = [M[0], M[1] + M[0]*x, M[2] + M[0]*y, M[3] + M[0]*z]
+    M = sp.MatrixSymbol("M", 4, 1)
+    x, y, z = sp.symbols("x y z")
+
+    answer = [M[0], M[1] + M[0] * x, M[2] + M[0] * y, M[3] + M[0] * z]
     result = gen.generate_M_shift_operators(order, (x, y, z), M_dict)
     for a, b in zip(answer, result):
         assert a == b
-    
+
 
 def test_generate_M_shift_operators_dipole():
     order = 2
     source = 1
-    M_dict = {(1, 0, 0): 0,
-              (0, 1, 0): 1,
-              (0, 0, 1): 2,
-              (2, 0, 0): 3,
-              (1, 1, 0): 4,
-              (1, 0, 1): 5,
-              (0, 2, 0): 6,
-              (0, 1, 1): 7,
-              (0, 0, 2): 8,
-              }
+    M_dict = {
+        (1, 0, 0): 0,
+        (0, 1, 0): 1,
+        (0, 0, 1): 2,
+        (2, 0, 0): 3,
+        (1, 1, 0): 4,
+        (1, 0, 1): 5,
+        (0, 2, 0): 6,
+        (0, 1, 1): 7,
+        (0, 0, 2): 8,
+    }
 
-    M = sp.MatrixSymbol('M', Nterms(order), 1)
-    x, y, z = sp.symbols('x y z')
+    M = sp.MatrixSymbol("M", Nterms(order), 1)
+    x, y, z = sp.symbols("x y z")
 
     # Hand computed for correctness
-    answer = [M[0],
-              M[1],
-              M[2],
-              M[0]*x + M[3],
-              M[0]*y + M[1]*x + M[4],
-              M[0]*z + M[2]*x + M[5],
-              M[1]*y + M[6],
-              M[1]*z + M[2]*y + M[7],
-              M[2]*z + M[8],
-              ]
+    answer = [
+        M[0],
+        M[1],
+        M[2],
+        M[0] * x + M[3],
+        M[0] * y + M[1] * x + M[4],
+        M[0] * z + M[2] * x + M[5],
+        M[1] * y + M[6],
+        M[1] * z + M[2] * y + M[7],
+        M[2] * z + M[8],
+    ]
 
-    result = gen.generate_M_shift_operators(order, (x, y, z), M_dict, source_order=source)
+    result = gen.generate_M_shift_operators(
+        order, (x, y, z), M_dict, source_order=source
+    )
     for i, (a, b) in enumerate(zip(answer, result)):
         assert a == b, f"i = {i}, a = {a}, b = {b}"
-
-    
-
-    
--- tests/test_field.py
+++ tests/test_field.py
@@ -2,31 +2,39 @@ import fmmgen
 import numpy as np
 from fmmgen.utils import Nterms
 import logging
-logging.getLogger('fmmgen')
+
+logging.getLogger("fmmgen")
 
 TOTALORDER = 4
+
 
 def test_monopole_M2P():
     source_order = 0
     order = source_order + TOTALORDER
     cse = True
     atomic = True
-    precision='double'
+    precision = "double"
 
-    fmmgen.generate_code(order, "MonopoleOrigin",
-                         precision=precision,
-                         CSE=cse,
-                         cython=True,
-                         potential=True,
-                         field=True,
-                         source_order=source_order,
-                         atomic=atomic, minpow=5,
-                         harmonic_derivs=True,
-                         language='c')
+    fmmgen.generate_code(
+        order,
+        "MonopoleOrigin",
+        precision=precision,
+        CSE=cse,
+        cython=True,
+        potential=True,
+        field=True,
+        source_order=source_order,
+        atomic=atomic,
+        minpow=5,
+        harmonic_derivs=True,
+        language="c",
+    )
 
     import pyximport
+
     pyximport.install()
     import MonopoleOrigin_wrap as fmm
+
     # Locate charge +q at (0, 0, d/2)
     # Locate charge -q at (0, 0, d/2)
     # Compute multipole at origin
@@ -39,12 +47,16 @@ def test_monopole_M2P():
     O = np.array([0.0, 0.0, 0.0])
 
     Rinv = 1 / np.linalg.norm(dx)
-    F_python = np.array([q*Rinv,
-                         q*dx[0]*(Rinv*Rinv*Rinv),
-                         q*dx[1]*(Rinv*Rinv*Rinv),
-                         q*dx[2]*(Rinv*Rinv*Rinv)])
+    F_python = np.array(
+        [
+            q * Rinv,
+            q * dx[0] * (Rinv * Rinv * Rinv),
+            q * dx[1] * (Rinv * Rinv * Rinv),
+            q * dx[2] * (Rinv * Rinv * Rinv),
+        ]
+    )
 
-    print(f'F_python = {F_python}')
+    print(f"F_python = {F_python}")
 
     F0_last = 100
     F1_last = 100
@@ -54,12 +66,12 @@ def test_monopole_M2P():
         S[0] = q
         M = np.zeros(Msize)
         F = np.zeros(4)
-        fmm.M2M(*(O-x1), S, M, Order)
+        fmm.M2M(*(O - x1), S, M, Order)
         print(M)
-        fmm.M2P(*(x2-O), M, F, Order)
-        print(f'Order = {Order}, F = {F}')
+        fmm.M2P(*(x2 - O), M, F, Order)
+        print(f"Order = {Order}, F = {F}")
 
-        assert np.abs((F[0] - F_python[0])/F_python[0]) < F0_last
-        assert np.abs((F[1] - F_python[1])/F_python[1]) < F1_last
+        assert np.abs((F[0] - F_python[0]) / F_python[0]) < F0_last
+        assert np.abs((F[1] - F_python[1]) / F_python[1]) < F1_last
         assert F[2] == 0
         assert F[3] == 0
